d0f7ec3dce9425faccd9bb59dcca8feb11c9bb88
Emre Akurek <emreyeprem@gmail.com>
2019-05-16 09:30:05 -0500

    displayedData

---
+++ src/utils/globalFunctions.js
@@ -0,0 +1,38 @@
+import meterData from './meterlist.json'
+// list of values of given key from each objects in an array
+const list_of_obj_val = function list_of_obj_val(array, key){
+  let list = array.map((each)=>{
+    return each[key]
+  })
+  return list
+}
+// remove double values from an array
+const remove_dublicate = function remove_dublicate(array){
+  let list_of_non_dublicate = [];
+  array.forEach((each)=>{
+      if(list_of_non_dublicate.includes(each)===false){
+        list_of_non_dublicate.push(each)
+      }
+  })
+  return list_of_non_dublicate
+}
+// filter an array by a given key value
+function filter_by_value(array, key , value){
+  let filtered_array = array.filter((each)=>{
+    return each[key]== value
+  })
+  return filtered_array
+}
+// sort an array of objects by a key value
+//function sort_by_value(array, )
+
+
+
+
+
+
+export {
+   list_of_obj_val,
+   remove_dublicate,
+   filter_by_value
+}
